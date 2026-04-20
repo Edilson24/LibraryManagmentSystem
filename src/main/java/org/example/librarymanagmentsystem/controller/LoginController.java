dbfbf03599095d65d7b71255d98925a115fd177f
Edilson24 <www.edilsonhilario@gmail.com>
2026-04-20 09:15:34 +0200

    tela de login funciondo

--- src/main/java/org/example/librarymanagmentsystem/controller/LoginController.java
+++ src/main/java/org/example/librarymanagmentsystem/controller/LoginController.java
@@ -2,10 +2,29 @@ package org.example.librarymanagmentsystem.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class LoginController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button close;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsuario;
 
     public void loginAdmin() {
 
